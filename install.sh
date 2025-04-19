0e047e5bd3c63b3d290e3a627612934cff665267
Skynet Technologies <developer3@skynettechnologies.com>
2025-04-19 13:03:02 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,4 +11,4 @@ FRONTEND_BASE="/usr/local/cpanel/base/frontend/$THEMENAME/all_in_one_accessibili
 /usr/local/cpanel/scripts/install_plugin /usr/local/cpanel/3rdparty/bin/cpanel-plugin-all-in-one-accessibility/configuration --theme="$THEMENAME"
 
 #copy_frontend
-cp -r "/usr/local/cpanel/3rdparty/bin/cpanel-plugin-all-in-one-accessibility/frontend" "$FRONTEND_BASE"
+cp -r /usr/local/cpanel/3rdparty/bin/cpanel-plugin-all-in-one-accessibility/frontend /usr/local/cpanel/base/frontend/$THEMENAME/all_in_one_accessibility
