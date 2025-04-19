8e0d0df30c2e85f691c6f7defdf17a4697b8162f
Skynet Technologies <developer3@skynettechnologies.com>
2025-04-19 13:04:35 +0530

    Update install.sh
    Debug

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 THEMENAME="jupiter"
-if [ "$1" ]
-then
-THEMENAME="$1"
-fi
+#if [ "$1" ]
+#then
+#THEMENAME="$1"
+#fi
 
-FRONTEND_BASE="/usr/local/cpanel/base/frontend/$THEMENAME/all_in_one_accessibility"
+#FRONTEND_BASE="/usr/local/cpanel/base/frontend/$THEMENAME/all_in_one_accessibility"
 
 #install
-/usr/local/cpanel/scripts/install_plugin /usr/local/cpanel/3rdparty/bin/cpanel-plugin-all-in-one-accessibility/configuration --theme="$THEMENAME"
+#/usr/local/cpanel/scripts/install_plugin /usr/local/cpanel/3rdparty/bin/cpanel-plugin-all-in-one-accessibility/configuration --theme="$THEMENAME"
 
 #copy_frontend
-cp -r /usr/local/cpanel/3rdparty/bin/cpanel-plugin-all-in-one-accessibility/frontend /usr/local/cpanel/base/frontend/$THEMENAME/all_in_one_accessibility
+#cp -r /usr/local/cpanel/3rdparty/bin/cpanel-plugin-all-in-one-accessibility/frontend /usr/local/cpanel/base/frontend/$THEMENAME/all_in_one_accessibility
